674976e03b3528dc47bdbb812206fa33fba88848
Eshan <eshangunsekara@gmail.com>
2025-10-20 17:13:20 +0530

    renamed data.js to data.jsx becuase js file contain html tags which needs to be jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
+import { useState } from "react";
 import Navigation from "./Navigation/nav"
 import Products from "./Products/Products"
+import products from "./db/data";
 import Recomended from "./Recomended/Recomended"
 import Sidebar from "./Sidebar/Sidebar"
+// import products from "./db/data" 
 
 function App() {
 
   return (
     <>
       <Sidebar />
-      <Navigation />
-      <Recomended />
-      <Products />
+      <Navigation/>
+      <Recomended/>
+      <Products/>
     </>
-  )
+  );
 }
 
 export default App
